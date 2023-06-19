24584f679300ac433d77455a46897b1b1928b1bc
Alessandro Arantes <alexskylark@gmail.com>
2023-06-19 11:23:45 -0300

    adjustments for targets = all

--- src/Handlebars.js
+++ src/Handlebars.js
@@ -117,8 +117,8 @@ export function registerHandlebarsHelpers() {
     );
 
     Handlebars.registerHelper({
-        eq: (v1, v2) => v1 === v2,
-        ne: (v1, v2) => v1 !== v2,
+        eq: (v1, v2) => v1 == v2,
+        ne: (v1, v2) => v1 != v2,
         lt: (v1, v2) => v1 < v2,
         gt: (v1, v2) => v1 > v2,
         lte: (v1, v2) => v1 <= v2,
